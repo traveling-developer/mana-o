be5cf1102ef230f2eecbcc3dd727f44ec9257618
Christoph Menzel <menzel@m3pro.fritz.box>
2024-05-18 18:44:21 +0200

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://dev.mana-o.info',
+  site: 'https://dev.mana-o.info',
+  integrations: [tailwind()]
 });

6085d717398b6fc813b6ded71a5d117e340a97b0
Christoph Menzel <menzel@m3pro.fritz.box>
2024-05-19 13:37:28 +0200

    Add font, logo and color and setup in tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				cinzel: ['Cinzel Decorative', 'serif'],
+			},
+			colors: {
+				'manao': '#fff7e3',
+			},
+		},
 	},
 	plugins: [],
 }
